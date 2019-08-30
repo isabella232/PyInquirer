9defcdf6320a3f358a12d32de83a859cab71b234
Simon Hewitt <si@sjhewitt.co.uk>
2019-08-30 10:22:31 -0700

    update input implementation

--- PyInquirer/prompts/__init__.py
+++ PyInquirer/prompts/__init__.py
@@ -1,2 +1,3 @@
 from PyInquirer.prompts import list
 from PyInquirer.prompts import confirm
+from PyInquirer.prompts import input
--- PyInquirer/prompts/input.py
+++ PyInquirer/prompts/input.py
@@ -2,50 +2,51 @@
 """
 `input` type question
 """
-from __future__ import print_function, unicode_literals
-import inspect
-from prompt_toolkit.token import Token
-from prompt_toolkit.shortcuts import create_prompt_application
-from prompt_toolkit.validation import Validator, ValidationError
-from prompt_toolkit.layout.lexers import SimpleLexer
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
-from .common import default_style
+import inspect
+from prompt_toolkit.styles import merge_styles
+from prompt_toolkit import PromptSession
+
+from prompt_toolkit.validation import Validator, ValidationError
+from PyInquirer.constants import DEFAULT_STYLE
 
 # use std prompt-toolkit control
 
 
-def question(message, **kwargs):
-    default = kwargs.pop('default', '')
-    validate_prompt = kwargs.pop('validate', None)
-    if validate_prompt:
-        if inspect.isclass(validate_prompt) and issubclass(validate_prompt, Validator):
-            kwargs['validator'] = validate_prompt()
-        elif callable(validate_prompt):
+def question(message,
+             qmark="?",
+             default='',
+             style=None,
+             validate=None,
+             **kwargs):
+
+    # TODO style defaults on detail level
+    kwargs['style'] = merge_styles([DEFAULT_STYLE, style])
+
+    if validate:
+        if inspect.isclass(validate) and issubclass(validate, Validator):
+            kwargs['validator'] = validate()
+        elif callable(validate):
             class _InputValidator(Validator):
                 def validate(self, document):
-                    verdict = validate_prompt(document.text)
-                    if not verdict == True:
-                        if verdict == False:
+                    verdict = validate(document.text)
+                    if verdict is not True:
+                        if verdict is False:
                             verdict = 'invalid input'
                         raise ValidationError(
                             message=verdict,
                             cursor_position=len(document.text))
             kwargs['validator'] = _InputValidator()
 
-    # TODO style defaults on detail level
-    kwargs['style'] = kwargs.pop('style', default_style)
-    qmark = kwargs.pop('qmark', '?')
-
-
-    def _get_prompt_tokens(cli):
+    def get_prompt_tokens():
         return [
-            (Token.QuestionMark, qmark),
-            (Token.Question, ' %s  ' % message)
+            ("class:qmark", qmark),
+            ("class:question", ' {} '.format(message)),
+            ("class:answer", default),
         ]
 
-    return create_prompt_application(
-        get_prompt_tokens=_get_prompt_tokens,
-        lexer=SimpleLexer(Token.Answer),
-        default=default,
-        **kwargs
-    )
+    return PromptSession(get_prompt_tokens, **kwargs).app
